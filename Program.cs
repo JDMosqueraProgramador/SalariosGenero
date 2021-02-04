c9592b542942b801a76dd5c2a0ec3d1989c0ce0a
JDMosqueraProgramador <53869985+JDMosqueraProgramador@users.noreply.github.com>
2021-02-04 09:00:53 -0500

    Primer commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace SalariosGenero
     {
         static void Main(string[] args)
         {
-            
+            // Variables
+
+            int cantHombres = 0;
+            int cantMujeres = 0;
+
+            // Proceso
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Ingrese el número de folio");
+                Console.ReadLine();
+
+                Console.WriteLine("Ingrese el sueldo que gana");
+                double sueldo = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Ingrese el sexo (femenino: 1, Masculino: 2");
+                double genero = Convert.ToDouble(Console.ReadLine());
+
+                if(genero == 2 && sueldo >= 900000)
+                {
+                    cantMujeres += 1;
+                }
+                else if(genero == 1 && sueldo <= 1000000)
+                {
+                    cantHombres += 1;
+                }
+
+            }
+
+            // Salida
+
+            Console.WriteLine($"La cantidad de mujeres que ganan más de 900.000 son: {cantMujeres}");
+            Console.WriteLine($"La cantidad de hombre que ganan menos de 1.000.000 son: {cantMujeres}");
+
         }
     }
 }
